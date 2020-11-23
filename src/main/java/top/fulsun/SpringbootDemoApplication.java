ed6645d973b14720f2e2b986519f88bf860d3fc1
fulsun <fl_6145@163.com>
2020-11-23 23:14:46 +0800

    Maven项目初始化

---
+++ src/main/java/top/fulsun/SpringbootDemoApplication.java
@@ -0,0 +1,17 @@
+package top.fulsun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springbootdemo
+ * @description: 项目的启动类
+ * @author: fulsun
+ * @create: 2020-11-23 16:04
+ **/
+@SpringBootApplication
+public class SpringbootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+}
